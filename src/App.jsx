300435acf866ddc0d790f3f7483c412e74821518
Abril Da Pra <abydapra@gmail.com>
2023-08-28 22:30:04 -0300

    Agrego páginas y navbar de libreria

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import './App.css';
-import Button from '@mui/material/Button';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import DeleteIcon from '@mui/icons-material/Delete';
-import IconButton from '@mui/material/IconButton';
-import Grid from '@mui/material';
+import ItemListContainer from './components/itemListContainer';
+import NavBar from './components/NavBar';
 
 function App() {
 
+  const greeting = "Bienvenidos a AVRIL accesorios";
+
   return (
     <>
-      <div>
-        <Button color="error" startIcon={<DeleteIcon />}>
-          Botoncito
-        </Button>
-        <DeleteIcon></DeleteIcon>
-        
-        <IconButton aria-label="ShoppingCartIcon">
-          <ShoppingCartIcon></ShoppingCartIcon>
-        </IconButton>
-
-        <Grid container>
-          <Grid item>
-
-          </Grid>
-        </Grid>
-
-      </div>
+      <NavBar />
+      <ItemListContainer greeting = {greeting} />
     </>
   )
 }
---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <>
+      Icono de carrito
+      <p>3</p>
+    </>
+  )
+}
+
+export default CartWidget
